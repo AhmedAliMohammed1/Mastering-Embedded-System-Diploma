8f1c85dd61918c338787edb28215745d6c5459ae
ahmed <medoocrcteano870@gmail.com>
2023-03-12 23:24:03 +0200

    HomeWork of Arrays&String Done

--- Unit 2 C Programming/Unit.2 Lesson.4 C Array & String/ex_3/src/ex_3.c
+++ Unit 2 C Programming/Unit.2 Lesson.4 C Array & String/ex_3/src/ex_3.c
@@ -12,6 +12,43 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	int arr[4][4],row,column,temp;
+	/*Get number of row and column */
+	printf("Enter rows and column of matrix");
+	scanf("%d %d",&row,&column);
+	/*Get elements of the matrix*/
+	printf("Enter elements of Matrix:\n");
+
+	for(int i=0;i<row;i++){
+		for(int j=0;j<column;j++){
+			printf("Enter elements of a%d%d:",(i+1),(j+1));
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	/*Print the matrix before transpose*/
+	printf("Entered  Matrix:\n");
+
+	for(int i=0;i<row;i++){
+		for(int j=0;j<column;j++){
+			printf("%d\t",arr[i][j]);
+		}
+		printf("\n");
+
+	}
+
+	/*Print the matrix after transpose*/
+	printf("Transpose  Matrix:\n");
+
+	for(int i=0;i<column;i++){
+		for(int j=0;j<row;j++){
+			printf("%d\t",arr[j][i]);
+		}
+		printf("\n");
+
+	}
+
+
+	return 0;
 }
