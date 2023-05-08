0fb914b0d17827ec8d905d8096abe14dfd626b1c
Ahmed <ahmed@ali.com>
2023-05-08 19:17:46 +0300

    lesson 3

---
+++ Unit 3 Embedded C/Lesson3/ASSEMPLY_StartUp/startup.s
@@ -0,0 +1,36 @@
+/* Assemply startup*/
+/*Eng: Ahmed Ali Mohammed*/
+
+.section .vector
+.word 0x20001000  /* Addres ->SP*/
+.word _vector_handler /* Reset Handler*/
+.word _vector_handler /* NMI*/
+.word _vector_handler //Hard Fault
+.word _vector_handler //MemManage
+.word _vector_handler //BusFault
+.word _vector_handler //UsageFault
+.word _vector_handler	//Reserved
+.word _vector_handler	//SVCall
+.word _vector_handler	//DebugMonitor
+.word _vector_handler	//Reserved
+.word _vector_handler	//PedSV
+.word _vector_handler	//SysTick
+.word _vector_handler	//WWDG
+.word _vector_handler	//PVD
+.word _vector_handler	//Tamper
+.word _vector_handler	//RTC
+.word _vector_handler	//Flash
+.word _vector_handler	//RCC
+.word _vector_handler	//EXTI0
+.word _vector_handler	//EXTI1
+.word _vector_handler	//EXTI2
+
+
+_reset:
+	bl main
+	b .
+	
+
+.thumb_func
+_vector_handler:
+	b _reset
---
+++ Unit 3 Embedded C/Lesson3/MakeFile/uart.h
@@ -0,0 +1,10 @@
+#ifndef __UART__H
+#define __UART__H
+
+void UART_sendString(char *str);
+
+
+
+
+
+#endif
