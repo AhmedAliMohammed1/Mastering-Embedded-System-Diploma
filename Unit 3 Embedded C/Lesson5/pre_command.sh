04967f0b68c2b46cb40cc42a6ccd4e2ff661b647
Ahmed <ahmed@ali.com>
2023-05-12 15:32:36 +0300

    Embedded C Done

---
+++ Unit 3 Embedded C/Lesson5/pre_command.sh
@@ -0,0 +1,4 @@
+export PATH=/c/ST/STM32CubeIDE_1.4.0/STM32CubeIDE/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.7-2018-q2-update.win32_1.4.0.202007081208/tools/bin:$PATH
+export PATH=/c/qemu/:$PATH
+export PATH=/c/Users/Ahmed/Desktop/Mastering-Embedded-System-Diploma/xpack-openocd-0.10.0-15/xpack-openocd-0.10.0-15/bin:$PATH
+
